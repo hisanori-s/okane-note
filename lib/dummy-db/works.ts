48f70f7794571f18458cb7b915a018806cc08195
d.w.child+github@gmail.com <d.w.child+github@gmail.com>
2024-10-27 00:13:01 +0900

    ダミー仕事情報の修正

--- lib/dummy-db/works.ts
+++ lib/dummy-db/works.ts
@@ -3,42 +3,32 @@ import type { Work, WorkSettings } from '@/types'
 export const dummyWorks: Work[] = [
   {
     id: 1,
-    title: "週末の大掃除",
+    title: "洗濯物をタンスに入れる",
     executionSpan: 'weekly',
-    executionDays: [6], // 土曜日
-    reward: 30,
+    executionDays: [1, 2, 3, 4, 5], // 平日
+    reward: 60,
     isValid: true,
-    note: "週末の大掃除タスク",
+    note: "自分のじゃなくても片付ける",
     completed: false
   },
   {
     id: 2,
-    title: "月初めの部屋の整理",
-    executionSpan: 'monthly',
-    executionDays: [1], // 毎月1日
-    reward: 50,
+    title: "靴を揃える",
+    executionSpan: 'weekly',
+    executionDays: [0, 1, 2, 3, 4, 5, 6], // 日曜から土曜まで毎日
+    reward: 80,
     isValid: true,
-    note: "月初めの大掃除",
+    note: "自分のじゃなくても片付けよう",
     completed: false
   },
   {
     id: 3,
-    title: "靴を揃える",
-    executionSpan: 'weekly',
-    executionDays: [0, 1, 2, 3, 4, 5, 6], // 日曜から土曜まで毎日
-    reward: 10,
-    isValid: true,
-    note: "毎日の習慣づけ",
-    completed: false
-  },
-  {
-    id: 4,
     title: "お風呂掃除",
     executionSpan: 'weekly',
     executionDays: [6, 0], // 土曜と日曜
-    reward: 20,
+    reward: 30,
     isValid: true,
-    note: "週末のお風呂掃除",
+    note: "お風呂の水を抜くだけ",
     completed: false
   }
 ]
